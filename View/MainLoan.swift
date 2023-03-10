48a7b541738f0f40081819dce2e4d11e991ca96b
Quantumzeed <87402893+Quantumzeed@users.noreply.github.com>
2023-03-10 11:59:30 +0700

    first
    awed

---
+++ View/MainLoan.swift
@@ -0,0 +1,128 @@
+//
+//  MainLoan.swift
+//  dokbea
+//
+//  Created by Quantum on 10/3/2566 BE.
+//
+
+import SwiftUI
+
+struct MainLoan: View {
+    
+//    init(){
+//        UISegmentedControl.appearance().selectedSegmentTintColor = .orange
+//    }
+    
+    // MARK: - Properties
+    @State var principal = ""
+    @State var interestRate = ""
+    @State var loanTerm = ""
+    @State var monthlyPayment = ""
+    @State var modeLoan : ModeLoan = .FindPayment
+    
+    enum ModeLoan: String , CaseIterable {
+        case FindPayment = "หาภาระต่องวด"
+        case FindTerm = "หาระยะเวลา"
+    }
+    
+    @State var test = "aa"
+    
+    var test2 = ["aa","bb"]
+    var body: some View {
+        NavigationView{
+            VStack{
+                
+                Picker("", selection: $modeLoan) {
+                    ForEach(ModeLoan.allCases , id: \.self) { item in
+                        Text(item.rawValue)
+                    }
+                }
+                .onChange(of: modeLoan, perform: { newValue in
+                    modeLoan == .FindPayment ? (monthlyPayment = "") : (loanTerm = "")
+                })
+                .pickerStyle(.segmented)
+                TextField("ยอดจัด", text: $principal)
+                    .textFieldStyle(CustomTextFieldStyle())
+                TextField("อัตราดอกเบี้ยต่อปี", text: $interestRate)
+                    .textFieldStyle(CustomTextFieldStyle())
+                TextField("ระยะเวลา", text: $loanTerm)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .disabled(modeLoan == .FindPayment ? false : true )
+                TextField("ภาระต่องวด", text: $monthlyPayment)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .disabled(modeLoan == .FindPayment ? true : false)
+                Button {
+                    getLoan()
+                } label: {
+                    Text("คำนวณ")
+                        .font(.title2)
+                }
+                .buttonStyle(.borderedProminent)
+
+                Spacer()
+            }
+            .padding()
+            .background(
+                Color.mint.gradient.opacity(0.1)
+            )
+            
+            .navigationBarTitle("คำนวณสินเชื่อ")
+        }
+    }
+    
+    func getLoan(){
+        
+        if modeLoan == .FindPayment {
+            getMonthlyPayment()
+        } else {
+            getLoanTerm()
+        }
+        
+    }
+    
+    func getMonthlyPayment(){
+        let r = (Double(interestRate) ?? 0) / 1200
+        let n = (Int(loanTerm) ?? 0)
+        let numerator = r * pow((1 + r), Double(n))
+        let denominator = pow((1 + r), Double(n)) - 1
+        let result = (Double(principal) ?? 0) * numerator / denominator
+        if result >= 1 {
+            monthlyPayment = String(result)
+        }
+        
+    }
+    
+    func getLoanTerm(){
+        let r = (Double(interestRate) ?? 0) / 1200
+        let denominator = round(-log((1-((Double(principal) ?? 0) * r) / (Double(self.monthlyPayment) ?? 0))) / (log(1+r)))
+        if denominator >= 1 {
+            self.loanTerm = String(denominator)
+        }
+    }
+    
+}
+
+
+
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .foregroundColor(.green)
+            .font(.title3)
+            .padding()
+            .background()
+            .cornerRadius(8)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .stroke(LinearGradient(colors: [.secondary.opacity(0.2),.secondary.opacity(0.25),.clear], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2)
+            }
+            
+    }
+}
+
+struct MainLoan_Previews: PreviewProvider {
+    static var previews: some View {
+        MainLoan()
+    }
+}
--- dokbeaApp.swift
+++ dokbeaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct dokbeaApp: App {
     var body: some Scene {
         WindowGroup {
-            LoanCalculator()
+            MainLoan()
         }
     }
 }
